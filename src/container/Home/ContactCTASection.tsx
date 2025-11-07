29cdf10a98358d7be409a7976e00657a371a54df
Supansa09 <161706116+Supansa09@users.noreply.github.com>
2025-11-07 14:56:57 +0700

    ่เ้รา

--- src/container/Home/ContactCTASection.tsx
+++ src/container/Home/ContactCTASection.tsx
@@ -2,10 +2,10 @@ import Link from 'next/link';
 
 export default function ContactCTASection() {
   return (
-    <section className="py-20 bg-gray-900 text-white">
+    <section className="py-20 relative bg-linear-to-br from-green-400 via-blue-600 to-purple-800 text-white">
             <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
               <h2 className="text-3xl md:text-4xl font-bold mb-4">
-                พร้อมที่จะเริ่มต้นโปรเจกต์ของคุณ?
+               พร้อมที่จะเริ่มต้นโปรเจกต์ของคุณ?
               </h2>
               
               <p className="text-xl mb-8 text-gray-300">
@@ -13,7 +13,7 @@ export default function ContactCTASection() {
               </p>
               <Link
                 href="/contact"
-                className="inline-block text-white px-8 py-3 rounded-lg font-semibold transition-all bg-linear-to-r from-yellow-500 via-yellow-500 to-orange-500 hover:from-yellow-600 hover:via-yellow-600 hover:to-orange-600 shadow-md hover:shadow-lg"
+                className="inline-block bg-yellow-500 text-white px-8 py-3 rounded-2xl font-semibold hover:bg-yellow-600  border-2 border-white/20 transition-all duration-300 hover:scale-110" 
               >
                 ติดต่อเรา
               </Link>
