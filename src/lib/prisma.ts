ba44a9101c66a6167f726da0bca7da99700f0a39
siwakorn <siwakornpoom190247@gmail.com>
2025-11-20 14:41:57 +0700

    lib

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,10 @@
-import { PrismaClient } from '../generated/prisma/client';
 
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
+import { PrismaClient } from '@prisma/client';
 
-export const prisma = global.prisma || new PrismaClient();
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-if (process.env.NODE_ENV !== 'production') {
-  global.prisma = prisma;
-}
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
